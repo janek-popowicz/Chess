403147a59533168ed809a20fe6d5a39231058f19
Benteqi <benedykt.cyran@gmail.com>
2025-03-11 22:19:59 +0100

    "Zoptymalizowany" enpassant

--- engine.py
+++ engine.py
@@ -97,16 +97,18 @@ def afterMove(turn:str, main_board, y1:int, x1:int, y2:int, x2:int)->str:
         if destination_tile.figure.type == 'p' :
             direction = 1 if destination_tile.figure.color == 'w' else -1
             if (destination_tile.y - start_tile.y) * direction == 2:
-                if destination_tile.x+direction >= 0:
-                    if main_board.board_state[destination_tile.y][destination_tile.x +direction].figure != None:
-                        if (main_board.board_state[destination_tile.y][destination_tile.x+direction].figure.type == 'p' 
-                        and main_board.board_state[destination_tile.y][destination_tile.x+direction].figure.color != destination_tile.figure.color):
-                            main_board.board_state[destination_tile.y][destination_tile.x+direction].figure.can_enpassant_l = True
-                if destination_tile.x+direction <= 7:
-                    if main_board.board_state[destination_tile.y][destination_tile.x -direction].figure != None:
-                        if (main_board.board_state[destination_tile.y][destination_tile.x-direction].figure.type == 'p' 
-                        and main_board.board_state[destination_tile.y][destination_tile.x-direction].figure.color != destination_tile.figure.color):
-                            main_board.board_state[destination_tile.y][destination_tile.x-direction].figure.can_enpassant_r = True
+                b = 1
+                for a in [0,-7]:
+                    if (destination_tile.x+direction )*b>= a:
+                        if main_board.board_state[destination_tile.y][destination_tile.x +direction].figure != None:
+                            if (main_board.board_state[destination_tile.y][destination_tile.x+direction].figure.type == 'p' 
+                            and main_board.board_state[destination_tile.y][destination_tile.x+direction].figure.color != destination_tile.figure.color):
+                                if b == 1:
+                                    main_board.board_state[destination_tile.y][destination_tile.x+direction].figure.can_enpassant_l = True
+                                else:
+                                    main_board.board_state[destination_tile.y][destination_tile.x+direction].figure.can_enpassant_r = True
+                    b = -1
+                    direction *= -1
            #Sprawdzanie promocji pionków
             if destination_tile.y in [0,7]:
                 return("promotion", destination_tile.y, destination_tile.x)
