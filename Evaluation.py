e06cab36186ad08f5d53ea311053f143cb12362d
Janek Popowicz <jan.j.popowicz@gmail.com>
2025-03-12 10:24:26 +0100

    Wyczyszczona ewaluacja

--- Evaluation.py
+++ Evaluation.py
@@ -1,55 +1,42 @@
 import board_and_fields
+import figures
 import engine
 
-
-class evaluation:
-    def __init__(self):
-        self.main_board = board_and_fields.Board()
+class Evaluation:
+    def __init__(self, main_board):
+        self.main_board = main_board
         self.waga_czarnych = 0
         self.waga_białych = 0
-        self.Pionek = 1
-        self.Skoczek = 3
-        self.Goniec = 3
-        self.Wieża = 5
-        self.Hetman = 9
-        self.Król = 1000
-    # w stoi za white a b za black
-    def ocena_materiału(self,): 
+        self.figures_values = {
+            'p': 1,
+            'N': 3,
+            'B': 3,
+            'R': 5,
+            'Q': 9,
+            'K': 1000
+        }
+
+    def ocena_materiału(self): 
         for i in range(8):
             for j in range(8):
-                pole = self.main_board[i][j]
-                pole = str(pole)
-                #print(f"Checking position ({i}, {j}): {pole}")  # Debugging print statement
-                print(pole)
-                if len(pole) < 2:
-                    continue  # Skip if the pole does not represent a piece
+                field = self.main_board.board_state[i][j]
+                if field.figure is None:
+                    continue  # Skip if the field does not have a piece
                 
-                if pole[1] == 'p':
-                    if pole[0] == 'w':
-                        self.waga_białych += self.Pionek
-                    else:
-                        self.waga_czarnych += self.Pionek
-                elif pole[1] == 'N':
-                    if pole[0] == 'w':
-                        self.waga_białych += self.Skoczek
-                    else:
-                        self.waga_czarnych += self.Skoczek
-                elif pole[1] == 'B':
-                    if pole[0] == 'w':
-                        self.waga_białych += self.Goniec
-                    else:
-                        self.waga_czarnych += self.Goniec
-                elif pole[1] == 'R':
-                    if pole[0] == 'w':
-                        self.waga_białych += self.Wieża
-                    else:
-                        self.waga_czarnych += self.Wieża
-                elif pole[1] == 'Q':
-                    if pole[0] == 'w':
-                        self.waga_białych += self.Hetman
-                    else:
-                        self.waga_czarnych += self.Hetman
+                figure = field.figure.return_figure()
+                print(figure)
+                
+                if len(figure) < 2:
+                    continue  # Skip if the figure representation is invalid
+                
+                piece_type = figure[1]
+                piece_color = figure[0]
+                
+                if piece_color == 'w':
+                    self.waga_białych += self.figures_values.get(piece_type, 0)
+                else:
+                    self.waga_czarnych += self.figures_values.get(piece_type, 0)
 
         print(f"Final weights - White: {self.waga_białych}, Black: {self.waga_czarnych}")  # Debugging print statement
         return [self.waga_białych, self.waga_czarnych]
-                
+
