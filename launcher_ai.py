7833ef88df27763f4dad69a899b5ffc42c153238
Janek Popowicz <jan.j.popowicz@gmail.com>
2025-03-22 22:40:18 +0100

    V

--- launcher_ai.py
+++ launcher_ai.py
@@ -1,4 +1,10 @@
 import algorithms.minimax as minimax
 
+import sys
+import os
 
-move = minimax.Minimax.get_best_move()
+# Dodaj katalog główny do sys.path, jeśli nie jest już w nim zawarty
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+minimax_object = minimax.Minimax()
+move = minimax_object.get_best_move()
