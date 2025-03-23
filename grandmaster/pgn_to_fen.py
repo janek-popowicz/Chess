8574844e1c79468de37922fb00fe6ce41239b37c
Janek Popowicz <jan.j.popowicz@gmail.com>
2025-03-23 21:29:06 +0100

    Naprawione crashowanie jak elo = ?

--- grandmaster/pgn_to_fen.py
+++ grandmaster/pgn_to_fen.py
@@ -32,8 +32,16 @@ def parse_pgn(pgn_text):
             headers[key] = value
         
         # Określenie, kto jest arcymistrzem, jeśli dostępne
-        white_elo = int(headers.get("WhiteElo", 0))
-        black_elo = int(headers.get("BlackElo", 0))
+        white_elo_str = headers.get("WhiteElo", "0")
+        black_elo_str = headers.get("BlackElo", "0")
+        
+        # Konwersja Elo na liczby, obsługa "?"
+        white_elo = int(white_elo_str) if white_elo_str.isdigit() else 0
+        black_elo = int(black_elo_str) if black_elo_str.isdigit() else 0
+        
+        print(f"White Elo: {white_elo}")
+        print(f"Black Elo: {black_elo}")
+        
         if white_elo or black_elo:
             last_grandmaster_color = "w" if white_elo > black_elo else "b"
         
@@ -50,32 +58,6 @@ def parse_pgn(pgn_text):
         })
     
     return extracted_games
-
-# Przykładowe użycie
-pgn_data = """[Event "Match (active)"]
-[Site "Creta (Greece)"]
-[Date "2003.??.??"]
-[Round "2"]
-[White "Azmaiparashvili Zurab (GEO)"]
-[Black "Kasparov Garry (RUS)"]
-[Result "0-1"]
-[ECO "D11"]
-[WhiteElo "2672"]
-[BlackElo "2813"]
-[Annotator ""]
-[Source ""]
-[Remark "IX"]
-
-1.c4 c6 2.d4 d5 3.Nf3 Nf6 4.e3 a6 5.Qc2 Bg4 6.Ne5 Bh5 7.Qb3 Qc7
-8.cxd5 cxd5 9.Nc3 e6 10.Bd2 Bd6 11.Rc1 Nc6 12.Na4 O-O 13.Nxc6
-bxc6 14.Qb6 Qe7 15.Bd3 Bg6 16.Bxg6 fxg6 17.f3 Ne4 18.fxe4 Qh4+
-19.g3 Qxe4 20.Ke2 Qg2+ 21.Kd3 Rf2 22.Qa5 Rb8 23.a3 Bc7 24.Qxc7
-Rxd2+ 25.Kc3 Rdxb2 0-1
-"""
-# result = parse_pgn(pgn_data)
-# for game in result:
-#     print(f"Arcymistrz grał kolorem: {game['grandmaster']}")
-#     print("Ruchy:", game["moves"])
 
 
 
@@ -105,14 +87,14 @@ def main():
                 # Pobierz aktualny FEN (bez liczników ruchów)
                 current_fen = board_to_fen_inverted(main_board, turn, y1, x1, y2, x2)
                 fen_parts = current_fen.split(' ')
-                position_fen = f"{fen_parts[0]} {fen_parts[1]} {fen_parts[2]} {fen_parts[3]}"
+                good_fen = f"{fen_parts[0]} {fen_parts[1]} {fen_parts[2]} {fen_parts[3]} 0 1"
                 
                 # Dodaj ruch do listy ruchów dla danego FENa
-                if position_fen in fen_moves:
-                    if current_move not in fen_moves[position_fen]:
-                        fen_moves[position_fen].append(current_move)
+                if good_fen in fen_moves:
+                    if current_move not in fen_moves[good_fen]:
+                        fen_moves[good_fen].append(current_move)
                 else:
-                    fen_moves[position_fen] = [current_move]
+                    fen_moves[good_fen] = [current_move]
             
             # Wykonaj ruch na planszy
             try:
